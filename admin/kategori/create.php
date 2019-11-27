afe4c7e848e616fa933a4e43ca34b471d867cf02
resawanhungkul1 <resawanhungkul@gmail.com>
2019-11-27 08:07:38 +0700

    blog

---
+++ admin/kategori/create.php
@@ -0,0 +1,44 @@
+<?php 
+session_start();
+if(!$_SESSION['login']){
+     echo "<script type='text/javascript'>
+    alert('Maaf  Anda Harus Login Terlebih Dahulu');
+    window.location='../login.php'</script>";
+}else{
+  include('../../config/database.php');
+  $user=new Database();
+  $user=mysqli_query($user->koneksi,
+    "SELECT*FROM users WHERE  password='$_SESSION[login]'");
+  //var_dump($_SESSION['login']);
+  $user=mysqli_fetch_array($user);
+
+
+ ?>
+
+
+<!-- Header -->
+<?php  include('../../layouts/includes/head.php') ?>
+<!-- End Header -->
+  <body class="app header-fixed sidebar-fixed aside-menu-fixed sidebar-lg-show">
+  <!-- Navbar -->
+  <?php  include('../../layouts/includes/navbar.php') ?>
+  <!-- End Navbar -->
+  <!-- sidebar -->
+  <?php  include('../../layouts/includes/sidebar.php') ?>
+  <!-- End sidebar -->
+    <div class="app-body">
+
+        <!-- Main content --> 
+        <main class="main"></main>
+        <!-- End Main content -->
+    </div>
+
+    <!-- CoreUI and necessary plugins-->
+<!-- Script -->
+ <?php  include('../layouts/includes/scripts.php') ?>
+<!-- End Script -->
+  </body>
+</html>
+<?php 
+}
+ ?>
---
+++ config/database.php
@@ -0,0 +1,21 @@
+<?php
+	class Database{
+		public  $host="localhost",$user ="root", $pass="",$db="blog-resawan";
+		public $koneksi;
+		public function __construct(){
+			$this->koneksi=mysqli_connect(
+				$this->host,
+				$this->user,
+				$this->pass,
+				$this->db
+				);
+			if ($this->koneksi) {
+				//echo"berhasil";
+
+			}else{
+				echo "koneksi gagal";
+			}
+		}
+	}  
+	$db=new  Database();
+?>
